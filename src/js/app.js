fa7f2b0951a1d50fc94f65ca990cd24a1b411da0
Юрий Кузмичев <kuzmichev@it-tian.ru>
2023-05-15 17:37:16 +0500

    add main section

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,17 @@
 import * as nav from './modules/nav.js';
 
 document.addEventListener('DOMContentLoaded', function () {
+  // Скролл для шапки
+  const header = document.querySelector('header');
+  function addClassScroll() {
+    if (window.pageYOffset > 100) {
+      header.classList.add('scroll');
+    } else {
+      header.classList.remove('scroll');
+    }
+  }
+  addClassScroll();
+  window.addEventListener('scroll', addClassScroll);
   // Скрываем меню
   nav.dropdownToggle();
   nav.hideItemMenu();
