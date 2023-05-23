cff5b8933b4fb804db7b532734c790947baeedeb
Юрий Кузмичев <kuzmichev@it-tian.ru>
2023-05-23 18:01:02 +0500

    minor fix

--- gulp/tasks/favicon.js
+++ gulp/tasks/favicon.js
@@ -1,43 +1,40 @@
-import favicons from "gulp-favicons";
-import filter from "gulp-filter";
+import favicons from 'gulp-favicons';
+import filter from 'gulp-filter';
 const config = {
   icons: {
-    favicons: true, 
-    appleIcon: true, 
-    android: true, 
-    windows: false, 
+    favicons: true,
+    appleIcon: true,
+    android: true,
+    windows: false,
     yandex: false,
     coast: false,
     firefox: false,
-    appleStartup: false
+    appleStartup: false,
   },
-  path: "img/favicon/"
+  path: 'assets/img/favicon/',
 };
 export const favicon = () => {
-  return app.gulp.src(`${app.path.src.favicon}`, {})
-    .pipe(app.plugins.plumber(
-      app.plugins.notify.onError({
-        title: "Favicon",
-        message: "Error <%= error.message %>"
-      }))
+  return app.gulp
+    .src(`${app.path.src.favicon}`, {})
+    .pipe(
+      app.plugins.plumber(
+        app.plugins.notify.onError({
+          title: 'Favicon',
+          message: 'Error <%= error.message %>',
+        })
+      )
     )
     .pipe(app.gulp.dest(`${app.path.build.favicon}`))
-    .pipe(favicons({
-      appName: `Template name`,
-      appShortName: `Template short-name`,
-      appDescription: `Template desc`,
-      icons: {
-        favicons: true, 
-        appleIcon: true, 
-        android: true, 
-        windows: false, 
-        yandex: false,
-        coast: false,
-        firefox: false,
-        appleStartup: false
-      },
-      path: "aasets/img/favicon/"
-    }))
+    .pipe(
+      favicons({
+        // Здесь импортировать из файла package.json
+        appName: `Template name`,
+        appShortName: `Template short-name`,
+        appDescription: `Template desc`,
+        icons: config.icons,
+        path: config.path,
+      })
+    )
     .pipe(app.gulp.dest(`${app.path.build.favicon}`))
     .pipe(filter(['favicon.ico', 'apple-touch-icon.png', 'manifest.json']))
     .pipe(app.gulp.dest(`${app.path.buildFolder}`));
--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,9 @@
 // Основной модуль
-import gulp from "gulp";
+import gulp from 'gulp';
 // Импорт путей
-import { path } from "./gulp/config/path.js";
+import { path } from './gulp/config/path.js';
 // Импорт общих плагинов
-import { plugins } from "./gulp/config/plugins.js";
+import { plugins } from './gulp/config/plugins.js';
 
 // Передаем значения в глобальную переменную
 global.app = {
@@ -12,37 +12,32 @@ global.app = {
   path: path,
   gulp: gulp,
   plugins: plugins,
-  favicon: favicon
+  favicon: favicon,
 };
 
 // Импорт задач
-import { copy } from "./gulp/tasks/copy.js";
-import { reset } from "./gulp/tasks/reset.js";
-import { html } from "./gulp/tasks/html.js";
-import { server } from "./gulp/tasks/server.js";
-import { scss } from "./gulp/tasks/scss.js";
-import { js } from "./gulp/tasks/js.js";
-import { images } from "./gulp/tasks/images.js";
-import { otfToTtf, ttfToWoff, fontsStyle } from "./gulp/tasks/fonts.js";
-import { svgSprive } from "./gulp/tasks/svgSprive.js";
-import { zip } from "./gulp/tasks/zip.js";
-import { ftp } from "./gulp/tasks/ftp.js";
-import { favicon } from "./gulp/tasks/favicon.js";
+import { copy } from './gulp/tasks/copy.js';
+import { reset } from './gulp/tasks/reset.js';
+import { html } from './gulp/tasks/html.js';
+import { server } from './gulp/tasks/server.js';
+import { scss } from './gulp/tasks/scss.js';
+import { js } from './gulp/tasks/js.js';
+import { images } from './gulp/tasks/images.js';
+import { otfToTtf, ttfToWoff, fontsStyle } from './gulp/tasks/fonts.js';
+import { svgSprive } from './gulp/tasks/svgSprive.js';
+import { zip } from './gulp/tasks/zip.js';
+import { ftp } from './gulp/tasks/ftp.js';
+import { favicon } from './gulp/tasks/favicon.js';
 
 // Наблюдатель за изменениями в файле
 function watcher() {
-  gulp.watch(path.watch.files, copy); 
-  // gulp.watch(path.watch.copy, gulp.series(copy, ftp))
-  gulp.watch(path.watch.html, html); 
+  gulp.watch(path.watch.files, copy);
+  gulp.watch(path.watch.html, html);
   // gulp.watch(path.watch.html, gulp.series(html, ftp))
-  gulp.watch(path.watch.scss, scss); 
-  // gulp.watch(path.watch.scss, gulp.series(scss, ftp))
-  gulp.watch(path.watch.js, js); 
-  // gulp.watch(path.watch.js, gulp.series(js, ftp))
-  gulp.watch(path.watch.images, images); 
-  // gulp.watch(path.watch.images, gulp.series(images, ftp))
-  gulp.watch(path.watch.favicon, favicon); 
-  // gulp.watch(path.watch.favicon, gulp.series(favicon, ftp))
+  gulp.watch(path.watch.scss, scss);
+  gulp.watch(path.watch.js, js);
+  gulp.watch(path.watch.images, images);
+  gulp.watch(path.watch.favicon, favicon);
 }
 
 export { svgSprive };
@@ -52,7 +47,6 @@ const fonts = gulp.series(otfToTtf, ttfToWoff, fontsStyle);
 
 // Основные задачи
 const mainTasks = gulp.series(fonts, gulp.parallel(copy, html, scss, js, images, favicon));
-// const mainTasks = gulp.parallel(copy, html, scss, js, images, favicon); 
 
 // Построение сценариев выполнения задач
 const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server));
@@ -61,11 +55,11 @@ const deployZIP = gulp.series(reset, mainTasks, zip);
 const deployFTP = gulp.series(reset, mainTasks, ftp);
 
 // Экспорт сценариев
-export { dev }
-export { build }
-export { deployZIP }
-export { deployFTP }
-export { favicon }
+export { dev };
+export { build };
+export { deployZIP };
+export { deployFTP };
+export { favicon };
 
 // Выполнение сценария по умолчанию
 gulp.task('default', dev);
