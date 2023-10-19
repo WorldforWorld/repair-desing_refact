e6ccb2825f820f557d1e70528b1d761255a3770c
Юрий Кузмичев <kuzmichev@it-tian.ru>
2023-10-19 17:53:25 +0500

    refactoring code

--- src/js/app.js
+++ src/js/app.js
@@ -1,22 +1,34 @@
-import * as nav from './modules/nav.js';
-import * as scroll from './modules/scroll-header.js';
-import * as init from './modules/map.js';
-import { slider } from './modules/projects-slider.js';
-import { isWebp } from './modules/functions.js';
+import { isWebp } from "./modules/functions.js";
+import * as init from "./modules/map.js";
+import * as nav from "./modules/nav.js";
+import { slider } from "./modules/projects-slider.js";
+import * as scroll from "./modules/scroll-header.js";
+import { scrollButtonTop } from "./modules/upbutton.js";
+import { maskPhone } from "./modules/validate_form/maskPhone.js";
+import { validateForms } from "./modules/validate_form/validate_forms.js";
+
 isWebp();
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function () {
+  validateForms(".control__form");
+  validateForms(".offer__form");
+  validateForms(".contacts__form");
+  validateForms(".popup__call__form");
+  // Маска для телефонов c валидацией
+  maskPhone("#user-phone");
+  // Добавление поведения к нопки вверх
+  scrollButtonTop("#upbutton");
   // Скролл для шапки
   scroll.addClassScroll();
-  window.addEventListener('scroll', scroll.addClassScroll);
+  window.addEventListener("scroll", scroll.addClassScroll);
 
   // Скрываем меню
   nav.dropdownToggle();
   nav.hideItemMenu();
-  window.addEventListener('resize', nav.hideItemMenu);
+  window.addEventListener("resize", nav.hideItemMenu);
 
   // Получаем все выпадающие списки
-  document.addEventListener('mouseover', nav.handleDropdownEvent);
-  document.addEventListener('click', nav.handleDropdownEvent);
+  document.addEventListener("mouseover", nav.handleDropdownEvent);
+  document.addEventListener("click", nav.handleDropdownEvent);
 
   // Бургер меню
   nav.burgerMenu();
