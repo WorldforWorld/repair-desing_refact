e92df4ef3dabc06ed234adaa6c06b0878b0c91dd
Юрий Кузмичев <kuzmichev@it-tian.ru>
2023-10-20 13:04:23 +0500

    add openModalPopup and add initMap

--- src/js/app.js
+++ src/js/app.js
@@ -1,18 +1,24 @@
 import { isWebp } from "./modules/functions.js";
-import * as init from "./modules/map.js";
+import { initMap } from "./modules/map.js";
 import * as nav from "./modules/nav.js";
+import { openModalPopup } from "./modules/popup_modal.js";
 import { slider } from "./modules/projects-slider.js";
 import * as scroll from "./modules/scroll-header.js";
 import { scrollButtonTop } from "./modules/upbutton.js";
 import { maskPhone } from "./modules/validate_form/maskPhone.js";
 import { validateForms } from "./modules/validate_form/validate_forms.js";
-
 isWebp();
 document.addEventListener("DOMContentLoaded", function () {
+  openModalPopup(
+    "[data-toggle=modal]",
+    ".popup__call",
+    ".popup__call .form__close"
+  );
   validateForms(".control__form");
   validateForms(".offer__form");
   validateForms(".contacts__form");
   validateForms(".popup__call__form");
+  new WOW().init();
   // Маска для телефонов c валидацией
   maskPhone("#user-phone");
   // Добавление поведения к нопки вверх
@@ -35,5 +41,5 @@ document.addEventListener("DOMContentLoaded", function () {
   // Инициализируем слайдер в секции projects
   slider();
   // Инициализируем карту
-  ymaps.ready(init.init);
+  initMap();
 });
