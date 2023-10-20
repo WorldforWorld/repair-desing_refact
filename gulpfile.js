20e7259a31c0425a5faaf1ec6e090a185014915c
Юрий Кузмичев <kuzmichev@it-tian.ru>
2023-10-20 12:59:38 +0500

    add library

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,33 @@
 // Основной модуль
-import gulp from 'gulp';
+import gulp from "gulp";
 // Импорт путей
-import { path } from './gulp/config/path.js';
+import { path } from "./gulp/config/path.js";
 // Импорт общих плагинов
-import { plugins } from './gulp/config/plugins.js';
+import { plugins } from "./gulp/config/plugins.js";
 
 // Передаем значения в глобальную переменную
 global.app = {
-  isBuild: process.argv.includes('--build'),
-  isDev: !process.argv.includes('--build'),
+  isBuild: process.argv.includes("--build"),
+  isDev: !process.argv.includes("--build"),
   path: path,
   gulp: gulp,
   plugins: plugins,
 };
 
 // Импорт задач
-import { copy } from './gulp/tasks/copy.js';
-import { reset } from './gulp/tasks/reset.js';
-import { html } from './gulp/tasks/html.js';
-import { server } from './gulp/tasks/server.js';
-import { scss } from './gulp/tasks/scss.js';
-import { js } from './gulp/tasks/js.js';
-import { images } from './gulp/tasks/images.js';
-import { otfToTtf, ttfToWoff, fontsStyle } from './gulp/tasks/fonts.js';
-import { svgSprive } from './gulp/tasks/svgSprive.js';
-import { zip } from './gulp/tasks/zip.js';
-import { ftp } from './gulp/tasks/ftp.js';
-import { favicon } from './gulp/tasks/favicon.js';
+import { copy } from "./gulp/tasks/copy.js";
+import { favicon } from "./gulp/tasks/favicon.js";
+import { fontsStyle, otfToTtf, ttfToWoff } from "./gulp/tasks/fonts.js";
+import { ftp } from "./gulp/tasks/ftp.js";
+import { html } from "./gulp/tasks/html.js";
+import { images } from "./gulp/tasks/images.js";
+import { js } from "./gulp/tasks/js.js";
+import { library } from "./gulp/tasks/library.js";
+import { reset } from "./gulp/tasks/reset.js";
+import { scss } from "./gulp/tasks/scss.js";
+import { server } from "./gulp/tasks/server.js";
+import { svgSprive } from "./gulp/tasks/svgSprive.js";
+import { zip } from "./gulp/tasks/zip.js";
 
 // Наблюдатель за изменениями в файле
 function watcher() {
@@ -36,17 +37,20 @@ function watcher() {
   gulp.watch(path.watch.scss, scss);
   gulp.watch(path.watch.js, js);
   gulp.watch(path.watch.images, images);
+  gulp.watch(path.watch.library, library);
 }
 
 // Отдельные сценарии
-export { svgSprive };
-export { favicon };
+export { favicon, svgSprive };
 
 // Последовательная обработка шрифтов
 const fonts = gulp.series(otfToTtf, ttfToWoff, fontsStyle);
 
 // Основные задачи
-const mainTasks = gulp.series(fonts, gulp.parallel(copy, html, scss, js, images));
+const mainTasks = gulp.series(
+  fonts,
+  gulp.parallel(copy, html, scss, js, images, library)
+);
 
 // Построение сценариев выполнения задач
 const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server));
@@ -55,10 +59,7 @@ const deployZIP = gulp.series(reset, gulp.parallel(mainTasks, favicon), zip);
 const deployFTP = gulp.series(reset, gulp.parallel(mainTasks, favicon), ftp);
 
 // Экспорт сценариев
-export { dev };
-export { build };
-export { deployZIP };
-export { deployFTP };
+export { build, deployFTP, deployZIP, dev };
 
 // Выполнение сценария по умолчанию
-gulp.task('default', dev);
+gulp.task("default", dev);
