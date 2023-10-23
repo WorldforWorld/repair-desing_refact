bde40e6390095c71f66908f91991a8c59d5a2ea0
Юрий Кузмичев <kuzmichev@it-tian.ru>
2023-10-23 10:03:45 +0500

    remove code

--- gulp/tasks/js.js
+++ gulp/tasks/js.js
@@ -1,33 +1,25 @@
 import webpack from "webpack-stream";
 
 export const js = () => {
-  return (
-    app.gulp
-      .src(app.path.src.js, { sourcemaps: app.isDev })
-      .pipe(
-        app.plugins.plumber(
-          app.plugins.notify.onError({
-            title: "JS",
-            message: "Error <%= error.message %>",
-          })
-        )
-      )
-      .pipe(
-        webpack({
-          mode: app.isBuild ? "production" : "development",
-          output: {
-            filename: "app.min.js",
-          },
+  return app.gulp
+    .src(app.path.src.js, { sourcemaps: app.isDev })
+    .pipe(
+      app.plugins.plumber(
+        app.plugins.notify.onError({
+          title: "JS",
+          message: "Error <%= error.message %>",
         })
       )
-      // .pipe(
-      //   app.plugins.if(
-      //     app.isBuild,
-      //     app.plugins.replace(/@img\//g, "/assets/img/")
-      //   )
-      // )
-      .pipe(app.plugins.replace(/@img\//g, "assets/img/"))
-      .pipe(app.gulp.dest(app.path.build.js))
-      .pipe(app.plugins.browsersync.stream())
-  );
+    )
+    .pipe(
+      webpack({
+        mode: app.isBuild ? "production" : "development",
+        output: {
+          filename: "app.min.js",
+        },
+      })
+    )
+    .pipe(app.plugins.replace(/@img\//g, "assets/img/"))
+    .pipe(app.gulp.dest(app.path.build.js))
+    .pipe(app.plugins.browsersync.stream());
 };
