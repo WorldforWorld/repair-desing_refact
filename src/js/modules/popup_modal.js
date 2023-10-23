9bf98ff9c16313699078f938f2827f421c128cd7
Юрий Кузмичев <kuzmichev@it-tian.ru>
2023-10-23 12:58:52 +0500

    add style overflow: hidden for body if has class "active"

--- src/js/modules/popup_modal.js
+++ src/js/modules/popup_modal.js
@@ -11,19 +11,25 @@
    );
  */
 export function openModalPopup(buttons, popup, closeButton) {
+  const body = document.querySelector("body");
   const modalBtns = document?.querySelectorAll(buttons);
   const modalPopup = document?.querySelector(popup);
   const modalPopupDialog = modalPopup?.querySelector(".popup__call__form");
   const closeBtn = document?.querySelector(closeButton);
   modalBtns?.forEach(button => {
-    button?.addEventListener("click", () => modalPopup?.classList?.add("open"));
+    button?.addEventListener("click", () => {
+      modalPopup?.classList?.add("open");
+      body.style.overflow = "hidden";
+    });
   });
-  closeBtn?.addEventListener("click", () =>
-    modalPopup?.classList?.remove("open")
-  );
+  closeBtn?.addEventListener("click", () => {
+    modalPopup?.classList?.remove("open");
+    body.style.overflow = "";
+  });
   modalPopup?.addEventListener("click", event => {
     if (!modalPopupDialog?.contains(event.target)) {
       modalPopup?.classList?.remove("open");
+      body.style.overflow = "";
     }
   });
 }
